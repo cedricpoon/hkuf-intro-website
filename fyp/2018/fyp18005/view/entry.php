643c7ee0cc92708b58d06f7db735f4df5bc53ea5
Cedric Poon <cedric@poons.me>
2018-09-30 22:59:16 +0800

    add icon

--- fyp/2018/fyp18005/view/entry.php
+++ fyp/2018/fyp18005/view/entry.php
@@ -2,7 +2,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="A HKU CS 2018/19 Final Year Project from fyp18005">
+
     <title>HKU Fellows</title>
+    <link rel=icon href=.\favicon.ico>
 
     <link rel="stylesheet" type="text/css" href=".\static\stylesheet\semantic.min.css">
     <script src=".\static\script\jquery.min.js"></script>
